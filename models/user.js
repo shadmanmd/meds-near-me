45e7e24a220c98551b76c32974f66c1ec7f86ecd
root <mdshadmanin99@gmail.com>
2020-06-06 14:09:03 +0000

    forgot password feature added

--- models/user.js
+++ models/user.js
@@ -2,9 +2,12 @@ var mongoose 				= require("mongoose"),
 	passportLocalMongoose 	= require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
+	email: {type: String, unique: true},
 	username: String,
 	password: String,
-	isAdmin: {type: Boolean, default: false}
+	isAdmin: {type: Boolean, default: false},
+	resetPasswordToken: String,
+	resetPasswordExpires: Date
 });
 
 UserSchema.plugin(passportLocalMongoose);
